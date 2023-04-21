5e37ca25969980d34058a015bdd7febd88bc956b
Jerrox19 <eyerusalemgirmachew@gmail.com>
2023-04-21 02:55:25 -0700

    requirement added

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# This manifest creates a file in /tmp with specific permissions, ownership, and content
+# This manifest creates a file in /tmp
 
 file { '/tmp/school':
   ensure => 'file',
   mode   => '0744',
   owner  => 'www-data',
   group  => 'www-data',
-  content => "I love Puppet\n",
+  content => "I love Puppet",
 }
-
