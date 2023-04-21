facd885a5e271dd0448f01ad9d9f02e462225056
Jerrox19 <eyerusalemgirmachew@gmail.com>
2023-04-21 02:59:04 -0700

    remove indentation

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This manifest creates a file in /tmp
 
-file { '/tmp/school':
-ensure => 'file',
-mode   => '0744',
-owner  => 'www-data',
-group  => 'www-data',
-content => "I love Puppet",
+file {'/tmp/school':
+ensure  => 'present',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
