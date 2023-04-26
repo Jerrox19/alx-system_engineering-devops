5c1883085606994b4fcfb167b9cc9b705957ff1c
Jerrox19 <eyerusalemgirmachew@gmail.com>
2023-04-26 00:43:30 -0700

    pp file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,18 @@
-# Puppet manifest to configure SSH client to use a private key and refuse password authentication
+# Puppet manifest to configure SSH client to use a private key
+# and refuse password authentication
+
+include stdlib
 
 file_line { 'Turn off passwd auth':
-  path => '/etc/ssh/sshd_config',
-  line => 'PasswordAuthentication no',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
-}
-
-service { 'ssh':
-  ensure => running,
-  enable => true,
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
