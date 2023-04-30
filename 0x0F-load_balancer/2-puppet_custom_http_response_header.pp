e936fa9c02d569ab981577df3a8382c8a7fd590d
Jerrox19 <eyerusalemgirmachew@gmail.com>
2023-04-30 15:41:48 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,8 @@
-# puppet scrit
-exec { 'install_nginx_and_configure_header':
-  command  => 'apt-get -y update && apt-get -y install nginx',
-  path     => '/usr/bin',
-  creates  => '/etc/nginx/sites-available/default',
-  notify   => Exec['add_custom_header'],
+# Puppet script for configuring nginx webserver
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
-
-exec { 'add_custom_header':
-  command  => '/bin/sed -i "/listen 80 default_server;/a add_header X-Served-By ${hostname};" /etc/nginx/sites-available/default',
-  path     => '/bin',
-  refreshonly => true,
-  subscribe => Exec['install_nginx_and_configure_header'],
-  require  => Class['nginx'],
-}
-
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Class['nginx'],
-}
-
